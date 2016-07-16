0f6800e8b2b98b21ac324cc12cdaf8f30ad57b68
Daniel Tomic <tom_danny2003@yahoo.com>
2016-07-16 11:30:43 -0400

    carousel added to the page

--- js/main.js
+++ js/main.js
@@ -9,3 +9,22 @@ $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+/********************************************
+CAROUSEL
+********************************************/
+$("#owl-demo").owlCarousel({
+
+    navigation: true, // Show next and prev buttons
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true
+
+    // "singleItem:true" is a shortcut for:
+    // items : 1, 
+    // itemsDesktop : false,
+    // itemsDesktopSmall : false,
+    // itemsTablet: false,
+    // itemsMobile : false
+
+});
